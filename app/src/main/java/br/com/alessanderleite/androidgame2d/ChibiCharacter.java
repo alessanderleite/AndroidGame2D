f39163e89b21ff6925b82be94c09dfc56d9550a2
alessanderleite <alessander.logistica@gmail.com>
2019-03-10 22:57:25 -0300

    declare all the variables and Objects inside ChibiCharacter class

--- app/src/main/java/br/com/alessanderleite/androidgame2d/ChibiCharacter.java
+++ app/src/main/java/br/com/alessanderleite/androidgame2d/ChibiCharacter.java
@@ -1,4 +1,32 @@
 package br.com.alessanderleite.androidgame2d;
 
+import android.graphics.Bitmap;
+
 public class ChibiCharacter extends GameObject {
+
+    private static final int ROW_TOP_TO_BOTTOM = 0;
+    private static final int ROW_RIGHT_TO_LEFT = 1;
+    private static final int ROW_LEFT_TO_RIGHT = 2;
+    private static final int ROW_BOTTOM_TO_TOP = 3;
+
+    //Row index of Image are being used.
+    private int rowUsing = ROW_LEFT_TO_RIGHT;
+
+    private int colUsing;
+
+    private Bitmap[] leftToRights;
+    private Bitmap[] rightToLefts;
+    private Bitmap[] topToBottoms;
+    private Bitmap[] bottomToTops;
+
+    //Velocity of game character (pixel/millisecond)
+    public static final float VELOCITY = 0.1f;
+
+    private int movingVectorX = 10;
+    private int movingVectorY = 5;
+
+    private long lastDrawNanoTime = -1;
+
+    private GameSurface gameSurface;
+
 }
